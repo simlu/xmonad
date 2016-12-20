a7d9359b63e1003568ff7881e5ed8a098c313369
simlu <simlu@users.noreply.github.com>
2016-12-20 13:17:22 -0800

    Create update-vb-guest-additons.sh

---
+++ scripts/update-vb-guest-additons.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# exit on error
+set -e
+
+# check if root
+if [ "$(whoami)" != "root" ] ; then
+    echo "please run as sudo"
+    exit;
+fi
+
+# run logic
+mkdir ~/tmp-mnt-cdrom
+sudo mount /dev/cdrom ~/tmp-mnt-cdrom
+sudo ~/tmp-mnt-cdrom/VBoxLinuxAdditions.run
+sudo umount ~/tmp-mnt-cdrom
+rm -rf ~/tmp-mnt-cdrom
