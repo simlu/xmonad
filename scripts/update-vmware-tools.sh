581e39e02ea99ae9655a046d413ea7754ef2a031
simlu <simlu@users.noreply.github.com>
2018-05-01 13:22:37 -0700

    Update update-vmware-tools.sh

--- scripts/update-vmware-tools.sh
+++ scripts/update-vmware-tools.sh
@@ -14,7 +14,7 @@ sudo mount /dev/cdrom ~/tmp-mnt-cdrom
 mkdir ~/tmp-vmware-tools-install
 tar -xzvf ~/tmp-mnt-cdrom/VMwareTools-*.tar.gz -C ~/tmp-vmware-tools-install
 (cd ~/tmp-vmware-tools-install/vmware-tools-distrib/ && sudo ./vmware-install.pl -f -d)
-umount ~/tmp-mnt-cdrom || /bin/true
+(umount ~/tmp-mnt-cdrom || /bin/true)
 rmdir ~/tmp-mnt-cdrom
 rm -rf ~/tmp-vmware-tools-install/vmware-tools-distrib/
 rmdir ~/tmp-vmware-tools-install
