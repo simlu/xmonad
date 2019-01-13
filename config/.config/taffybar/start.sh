5b4cb3cf7ef8a7ceb2179130cde96affae044a97
simlu <simlu@users.noreply.github.com>
2019-01-12 21:39:05 -0800

    Delete start.sh

--- config/.config/taffybar/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# start taffybar
-if [ -z "$(pgrep -f taffybar-linux)" ] ; then
-  echo "----------------" >> ~/.config/taffybar/debug.log &
-  echo "$(date)" >> ~/.config/taffybar/debug.log &
-  taffybar &>> ~/.config/taffybar/debug.log &
-fi
